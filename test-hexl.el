76caad5cb486b4ac35a16d626cd4b63ccb2622d9
Alexandre Adolphe <Alexandre_Adolphe@sigmadesigns.com>
2018-01-16 18:40:25 +0100

    Add files to test hexl mode.

---
+++ test-hexl.el
@@ -0,0 +1,56 @@
+
+
+
+(mapconcat (lambda (c) (regexp-quote (string c))) "pqr"
+                             "\\(?:\n\\(?:[:a-f0-9]+ \\)+ \\)?")
+"p\\(?:
+\\(?:[:a-f0-9]+ \\)+ \\)?q\\(?:
+\\(?:[:a-f0-9]+ \\)+ \\)?r"
+
+
+(overlay-get 'hexl-ascii-overlay 'face)
+
+
+
+(defface hexn-ascii-overlay-face
+  '((t (:inherit highlight)))
+  "Face for ascii overlay."
+  :group 'hexl)
+
+(custom-set-faces
+ `(hexn-ascii-overlay-face ((t (:box (:line-width -3 :color ,(face-background 'cursor)))))))
+
+(custom-set-faces
+ `(hexn-ascii-overlay-face ((t (:box (:line-width (-1 . -1) :color ,(face-background 'cursor)))))))
+
+
+(defun hexn-follow-ascii (&optional arg)
+  "Toggle following ASCII in Hexl buffers.
+With prefix ARG, turn on following if and only if ARG is positive.
+When following is enabled, the ASCII character corresponding to the
+element under the point is highlighted.
+Customize the variable `hexl-follow-ascii' to disable this feature."
+  (interactive "P")
+  (let ((on-p (if arg
+		  (> (prefix-numeric-value arg) 0)
+	       (not hexl-ascii-overlay))))
+    (message "TOTOOTOTOTO")
+    (if on-p
+      ;; turn it on
+      (if (not hexl-ascii-overlay)
+	  (progn
+	    (setq hexl-ascii-overlay (make-overlay 1 1)
+		  hexl-follow-ascii t)
+	    (overlay-put hexl-ascii-overlay 'face 'hexn-ascii-overlay-face)
+	    (add-hook 'post-command-hook 'hexl-follow-ascii-find nil t)))
+      ;; turn it off
+      (if hexl-ascii-overlay
+	  (progn
+	    (delete-overlay hexl-ascii-overlay)
+	    (setq hexl-ascii-overlay nil
+		  hexl-follow-ascii nil)
+	    (remove-hook 'post-command-hook 'hexl-follow-ascii-find t)
+	    )))))
+
+(advice-add 'hexl-follow-ascii :override 'hexn-follow-ascii)
+;; (define-advice hexl-follow-ascii (:override LAMBDA-LIST &optional NAME DEPTH) &rest BODY)
