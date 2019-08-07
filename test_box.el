04976c4e4ff81b2e05f018af043a97fdffa1868d
Alexandre Adolphe <alexandre.adolphe@gmail.com>
2019-08-08 00:38:58 +0200

    commit all new tests.

--- test_box.el
+++ test_box.el
@@ -4,8 +4,8 @@
   (save-excursion
     (goto-char (point-min))
     (insert "ABCDE\nABCDE\nABCDE\n")
-    ;; (put-text-property 8 11 'font-lock-face `(:box (:line-width ,s :color "red")))
-    (put-text-property 8 11 'font-lock-face `(:box (:line-width ,s :color "white" :style released-button)))
+    (put-text-property 8 11 'font-lock-face `(:box (:line-width ,s :color "red")))
+    ;; (put-text-property 8 11 'font-lock-face `(:box (:line-width ,s :color "white" :style released-button)))
     ))
 
 (test_box_around_text 0)
@@ -33,3 +33,13 @@
       "splash.svg" nil nil :relief 12))))
 
 (test_image_relief)
+
+(custom-set-faces  '(iedit-occurrence ((t (:inherit nil :box -1)))))
+(custom-set-faces  '(iedit-occurrence ((t (:inherit nil :box 1)))))
+(custom-set-faces  '(iedit-occurrence ((t (:inherit nil :box t)))))
+(custom-set-faces  '(iedit-occurrence ((t (:inherit nil :box)))))
+(custom-set-faces  '(iedit-occurrence ((t (:inherit nil :box nil)))))
+(custom-set-faces  '(iedit-occurrence ((t (:inherit nil :box (-1 . -1))))))
+(custom-set-faces  '(iedit-occurrence ((t (:inherit nil :box "red")))))
+(custom-set-faces  '(iedit-occurrence ((t (:inherit nil :box (:line-width -1 :color "red"))))))
+(custom-set-faces  '(iedit-occurrence ((t (:inherit nil :box (:line-width (-1 . -1) :color "red"))))))
